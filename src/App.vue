f047671c3b186a47e15d2ff25e361c02ce687330
829_100008418348_BBiiaoao <829_100008418348_BBiiaoao@git.cloud.tencent.com>
2018-12-24 22:21:38 +0800

    zb

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,50 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>hhhh</h1>
+    <div ref="chartsContainer" class="chartsContainer">
+      <h1>jj</h1>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import echarts from 'echarts';
+
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      voltage:[],
+      frequency:[],
+      current:[],
+      activePower:[],
+      reactivePower:[]
+    }
+  },
+  created() {
+      this.$axios.get("120.78.191.131:8080/arm2web/socket/json")
+              .then(res=>{
+                console.log(res);
+              })
+  },
+  mounted() {
+    // 基于准备好的dom，初始化echarts实例
+    var myChart = echarts.init(this.$refs.chartsContainer);
+  // 绘制图表
+    myChart.setOption({
+      title: { text: 'voltage' },
+      tooltip: {},
+      xAxis: {
+        data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+      },
+      yAxis: {},
+      series: [{
+        name: '销量',
+        type: 'line',
+        data: [5, 20, 36, 10, 10, 20]
+      }]
+    });
   }
 }
 </script>
@@ -25,4 +58,8 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+  .chartsContainer{
+    width:80%;
+    height: 400px;
+  }
 </style>
